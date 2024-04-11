8d2fc71f44fb71a89bb7775527207bfeb8dc4c20
Ramazan Ozer <ozerramazan991@gmail.com>
2024-04-11 13:29:28 +0300

    Initial Commit

---
+++ BreakingBadQuotes/BreakingBadQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadQuotesApp.swift
+//  BreakingBadQuotes
+//
+//  Created by Ramazan Ozer on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
